5c58a03580ab10a240761e0042644c71bc1391f8
lmttlw <2958756663@qq.com>
2026-04-18 14:56:23 +0800

    初始化项目

--- TestDemo1/Form1.cs
+++ TestDemo1/Form1.cs
@@ -19,7 +19,7 @@ namespace TestDemo1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello");
+            textBox1.Text = "hello";
         }
     }
 }
