cad49d0fc81935581f9464640ab4d66754f02865
lmttlw <2958756663@qq.com>
2026-04-18 15:00:10 +0800

    1

--- TestDemo1/Form1.Designer.cs
+++ TestDemo1/Form1.Designer.cs
@@ -34,11 +34,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 160);
+            this.button1.Location = new System.Drawing.Point(91, 166);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 56);
             this.button1.TabIndex = 0;
-            this.button1.Text = "测试";
+            this.button1.Text = "第二次测试";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
